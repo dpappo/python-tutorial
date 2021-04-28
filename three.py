def68539ae473949e688f543704ab813460851f2
Dan Pappo <dpappo@gmail.com>
2021-04-28 11:24:39 -0400

    booleans and comparison

---
+++ three.py
@@ -0,0 +1,15 @@
+# booleans are capitalized
+is_true = False
+
+# conditional statement:
+if is_true:
+    print("It is really true!")
+else:
+    print("It is actually not true")
+
+if ('a' == 'a'):
+    print("python uses double equals for comparison")
+
+# booleans can be coerced, with True == 1 and False == 0
+# don't actually write code like the next line, it is bad, but makes a point
+print(9 + True)

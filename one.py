336aa03c760d0a2d4df2661f68e233a31dda98e7
Dan Pappo <dpappo@gmail.com>
2021-04-28 10:47:08 -0400

    strings

--- one.py
+++ one.py
@@ -1,2 +1,16 @@
+# math operators built in
+# dynamically typed
 result = 1 + 2
-print(result)
+
+multi_line_string = """Line One
+Line Two
+Line Three"""
+
+# strings can be repeated without loops
+#print(multi_line_string * 2)
+
+# methods on strings
+# print(multi_line_string.lower())
+
+#built in functions
+print(len(multi_line_string))
